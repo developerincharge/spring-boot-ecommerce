f4b670ef885ea7d6552e0c4809fe65608b33f980
sabur786 <imtiazrizvi786@yahoo.com>
2025-04-16 15:14:36 -0500

    Update Repositories for cors and query

--- src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
+++ src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -3,6 +3,8 @@ package com.luv2code.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class SpringBootEcommerceApplication {
 
